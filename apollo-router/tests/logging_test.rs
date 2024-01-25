744854009d0eb4047f7c8578d6fe6e6531fbd596
o0Ignition0o <jeremy.lempereur@gmail.com>
2024-01-25 13:24:44 +0100

    remove test that has been transformed to a unit test

--- apollo-router/tests/logging_test.rs
+++ apollo-router/tests/logging_test.rs
@@ -8,16 +8,10 @@ use std::sync::Arc;
 use std::sync::Mutex;
 
 use apollo_router::graphql;
-use apollo_router::services::router;
-use apollo_router::services::supergraph;
 use serde_json::json;
-use tower::ServiceExt;
-use tracing::field;
 use tracing::Level;
 use tracing::Metadata;
 use tracing::Subscriber;
-use tracing_core::dispatcher;
-use tracing_core::Dispatch;
 use tracing_subscriber::registry::LookupSpan;
 use tracing_subscriber::Registry;
 
@@ -70,137 +64,6 @@ impl<'a> LookupSpan<'a> for TestLogSubscriber {
     }
 }
 
-async fn setup_router(config: serde_json::Value) -> router::BoxCloneService {
-    apollo_router::TestHarness::builder()
-        .with_subgraph_network_requests()
-        .configuration_json(config)
-        .unwrap()
-        .schema(include_str!("fixtures/supergraph.graphql"))
-        .build_router()
-        .await
-        .unwrap()
-}
-
-async fn query_with_router(
-    router: router::BoxCloneService,
-    request: supergraph::Request,
-) -> graphql::Response {
-    serde_json::from_slice(
-        router
-            .oneshot(request.try_into().unwrap())
-            .await
-            .unwrap()
-            .next_response()
-            .await
-            .unwrap()
-            .unwrap()
-            .to_vec()
-            .as_slice(),
-    )
-    .unwrap()
-}
-
-#[derive(Default, Clone, PartialEq, Debug)]
-struct LoggingCount {
-    supergraph_request_headers_count: usize,
-    supergraph_request_body_count: usize,
-    supergraph_response_body_count: usize,
-    supergraph_response_headers_count: usize,
-    subgraph_request_body_count: usize,
-    subgraph_request_headers_count: usize,
-    subgraph_response_body_count: usize,
-    subgraph_response_headers_count: usize,
-}
-
-impl LoggingCount {
-    const RESPONSE_BODY: &'static str = "http.response.body";
-    const RESPONSE_HEADERS: &'static str = "http.response.headers";
-    const REQUEST_HEADERS: &'static str = "http.request.headers";
-    const REQUEST_BODY: &'static str = "http.request.body";
-
-    fn count(&mut self, fields: &field::FieldSet) {
-        let fields_name: Vec<&str> = fields.iter().map(|f| f.name()).collect();
-        if fields_name.contains(&"apollo.subgraph.name") {
-            if fields_name.contains(&Self::RESPONSE_BODY) {
-                self.subgraph_response_body_count += 1;
-            }
-            if fields_name.contains(&Self::RESPONSE_HEADERS) {
-                self.subgraph_response_headers_count += 1;
-            }
-            if fields_name.contains(&Self::REQUEST_HEADERS) {
-                self.subgraph_request_headers_count += 1;
-            }
-            if fields_name.contains(&Self::REQUEST_BODY) {
-                self.subgraph_request_body_count += 1;
-            }
-        } else {
-            if fields_name.contains(&Self::RESPONSE_BODY) {
-                self.supergraph_response_body_count += 1;
-            }
-            if fields_name.contains(&Self::RESPONSE_HEADERS) {
-                self.supergraph_response_headers_count += 1;
-            }
-            if fields_name.contains(&Self::REQUEST_HEADERS) {
-                self.supergraph_request_headers_count += 1;
-            }
-            if fields_name.contains(&Self::REQUEST_BODY) {
-                self.supergraph_request_body_count += 1;
-            }
-        }
-    }
-}
-
-#[tokio::test(flavor = "multi_thread")]
-async fn simple_query_should_display_logs_for_subgraph_and_supergraph() {
-    let logging_config = serde_json::json!({
-        "experimental_when_header": [{
-            "name": "custom-header",
-            "match": "^foo.*",
-            "headers": true
-        }]
-    });
-    let request = supergraph::Request::fake_builder()
-        .header("custom-header", "foobar")
-        .query(r#"{ topProducts { upc name reviews {id product { name } author { id name } } } }"#)
-        .variable("topProductsFirst", 2_i32)
-        .variable("reviewsForAuthorAuthorId", 1_i32)
-        .build()
-        .expect("expecting valid request");
-
-    let event_store = Arc::new(Mutex::new(Vec::new()));
-    dispatcher::set_global_default(Dispatch::new(TestLogSubscriber {
-        event_metadata: event_store.clone(),
-        registry: Registry::default(),
-    }))
-    .expect("subscriber must be set");
-
-    let router = setup_router(serde_json::json!({"telemetry": {
-    "apollo": {
-            "schema_id": ""
-        },
-    "exporters": {
-        "tracing": {},
-        "logging": logging_config,
-        }}}))
-    .await;
-    let actual = query_with_router(router, request).await;
-
-    assert_eq!(0, actual.errors.len());
-    let mut logging_count = LoggingCount::default();
-    for event in &*event_store.lock().unwrap() {
-        logging_count.count(event.fields());
-    }
-
-    assert_eq!(logging_count.supergraph_request_headers_count, 1);
-    assert_eq!(logging_count.supergraph_request_body_count, 0);
-    assert_eq!(logging_count.supergraph_response_body_count, 0);
-    assert_eq!(logging_count.supergraph_response_headers_count, 1);
-    assert_eq!(logging_count.subgraph_response_body_count, 0);
-    assert_eq!(logging_count.subgraph_response_headers_count, 4);
-    assert_eq!(logging_count.subgraph_request_headers_count, 4);
-    assert_eq!(logging_count.subgraph_request_body_count, 0);
-}
-
 #[tokio::test(flavor = "multi_thread")]
 async fn display_headers_and_body_works_for_subgraph_and_source_api() {
     if std::env::var("TEST_APOLLO_KEY").is_ok() && std::env::var("TEST_APOLLO_GRAPH_REF").is_ok() {
@@ -230,7 +93,6 @@ async fn display_headers_and_body_works_for_subgraph_and_source_api() {
             ]))
             .await;
 
-        // TODO: probably perf low hanging fruits in the way schemas are parsed / generated.
         tokio::time::sleep(std::time::Duration::from_secs(1)).await;
         router.assert_started().await;
 
